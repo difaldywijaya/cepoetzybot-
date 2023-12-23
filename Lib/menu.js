777730c6e22fe50594813d77262f35165dcc9289
difaldywijaya <153864391+difaldywijaya@users.noreply.github.com>
2023-12-23 21:14:32 +0700

    Update menu.js

--- Lib/menu.js
+++ Lib/menu.js
@@ -1,135 +1,135 @@
 exports.menu = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `⚜*${BotName}*⚜
+	return `⚜*${Cepoetzybot}*⚜
   
 HALO *${id.split("@s.whatsapp.net")[0]}* 
 APA KABAR? SEMOGA BAIK-BAIK SAJA!
-Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* 😁
+Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!save wa saya biar bisa* 😁
 
 💵TOLONG BANTU DONASI AGAR BOT INI BISA TERUS BERJALAN💵
 💵GUNAKAN COMMAND !donasi untuk berdonasi💵
 
-📅*${tampilTanggal}* ⚡️
-⏰*${tampilWaktu}* ⚡️
+📅*${tampilTanggal}* 🤖
+⏰*${tampilWaktu}* 🤖
 (Waktu Server)
 
-🗒KUMPULAN COMMAND ${BotName}:
+🗒KUMPULAN COMMAND ${Cepoetzybot}:
  
-🐼*FUN*🐼
+😂*FUN*😂
 
-🐼 _*!pantun*_
-_${BotName}_🍄akan mengirimkanmu pantun secara random
+👾 _*!pantun*_
+_${Cepoetzybot}_👾akan mengirimkanmu pantun secara random
 
-🐼 _*!animepict*_
-_${BotName}_🍄akan mengirimkanmu gambar anime secara random
+👾 _*!animepict*_
+_${Cepoetzybot}_👾akan mengirimkanmu gambar anime secara random
 
-🐼 _*!sticker*_
-_${BotName}_🍄akan membuatkan sticker dari gambar yang kamu kirimkan
+👾 _*!sticker*_
+_${Cepoetzybot}_👾akan membuatkan sticker dari gambar yang kamu kirimkan
 
 Pengggunaan : Kirimkan gambarmu dengan caption !sticker
 
-🐼 _*!nulis <teks>*_
-_${BotName}_🍄akan menuliskan teks yang kamu kirimkan
+👾 _*!nulis <teks>*_
+_${Cepoetzybot}_👾akan menuliskan teks yang kamu kirimkan
 
 Contoh: !nulis pasti nana bangka dadang ko bang jamping jamping 
 
-🐼 _*!quotes*_
-_${BotName}_🍄akan mencarikanmu quotes secara random
+👾 _*!quotes*_
+_${Cepoetzybot}_👾akan mencarikanmu quotes secara random
 
-🐼 _*!pict <cewek/cowok>*_
-_${BotName}_🍄akan mengirimkanmu gambar cewek/cowok secara random
+👾 _*!pict <cewek/cowok>*_
+_${Cepoetzybot}_👾akan mengirimkanmu gambar cewek/cowok secara random
 Contoh: !pict cowok
 
-🐼 _*!animepict*_
-_${BotName}_🍄akan mengirimkanmu gambar anime secara random
+👾 _*!animepict*_
+_${Cepoetzybot}_👾akan mengirimkanmu gambar anime secara random
 
-🐼 _*!say <teks>*_
-_${BotName}_🍄akan mengirimkan kembali teks yang kamu kirimkan
+👾 _*!say <teks>*_
+_${Cepoetzybot}_👾akan mengirimkan kembali teks yang kamu kirimkan
 Contoh: !say buset BRO LU GANTENG BANGET AWOKWOKWOKWOWKWOK
 
-🐼 _*!lirik*_ <penyanyi-judul lagu>
-_${BotName}_🍄akan mengirimkanmu lirik lagu yang kamu inginkan
+👾 _*!lirik*_ <penyanyi-judul lagu>
+_${Cepoetzybot}_👾akan mengirimkanmu lirik lagu yang kamu inginkan
 contoh : !lirik Lisa-Gurenge
 
-🐼 _*!alay*_ <teks>
-_${BotName}_🍄akan mengubah teks yang kamu kirimkan menjadi alay
+👾 _*!alay*_ <teks>
+_${Cepoetzybot}_👾akan mengubah teks yang kamu kirimkan menjadi alay
 contoh : !alay ampun bang jago
 
 
 ☪*ISLAM*☪
 
-🐼 _*!sholat <daerah>*_
-_${BotName}_🍄akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
+👾 _*!sholat <daerah>*_
+_${Cepoetzybot}_👾akan mengirimkan jadwal sholat sesuai dengan daerah yang kamu kirimkan
 
 Penggunaan 	: !sholat + daerah kamu
 Contoh		: !sholat Surabaya
 
-🐼 _*!quran*_
-_${BotName}_🍄akan mengirimkanmu ayat Al-Quran secara random
+👾 _*!quran*_
+_${Cepoetzybot}_👾akan mengirimkanmu ayat Al-Quran secara random
 
 📥*DOWNLOADER*📥
 
-🐼 _*!yt <link>*_
-_${BotName}_🍄akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
+👾 _*!yt <link>*_
+_${Cepoetzybot}_👾akan mendownloadkan video youtube sesuai dengan link yang kamu kirimkan.
 Contoh: !yt https://youtu.be/linkamu
 
-🐼 _*!ytmp3 <link>*_
-_${BotName}_🍄akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
+👾 _*!ytmp3 <link>*_
+_${Cepoetzybot}_👾akan mengubah video youtube menjadi audio sesuai dengan link yang kamu kirimkan
 
 Contoh: !ytmp3 https://youtu.be/linkamu
 
-🐼 _*!ig <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+👾 _*!ig <link>*_
+_${Cepoetzybot}_👾akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !ig https://instagram.com/linkamu
 
-🐼 _*!fb <link>*_ //ERROR//
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+👾 _*!fb <link>*_ //ERROR//
+_${Cepoetzybot}👾_akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !fb https://facebook.com/linkamu
 
-🐼 _*!twt <link>*_
-_${BotName}_🍄akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
+👾 _*!twt <link>*_
+_${Cepoetzybot}_👾akan mengirimkanmu foto/video dari link ig yang kamu kirimkan
 
 Contoh: !twt https://twitter.com/linkamu
 
 🎭 _*!tiktok <link>*_
 
-_${BotName}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
+_${Cepoetzybot}_ akan mengirimkanmu video dari link tiktok yang kamu kirimkan
 
 🖥*EDUKASI*🖥
 
-🐼 _*!wikia <yang kamu cari>*_
-_${BotName}_🍄akan mencarikan yang kamu mau di wikipedia
+👾 _*!wikia <yang kamu cari>*_
+_${Cepoetzybot}_👾akan mencarikan yang kamu mau di wikipedia
 
 Contoh: !wikia Adolf Hitler
 
-🐼 _*!covid*_
-_${BotName}_🍄akan menampilkan data tentang COVID-19 di Indonesia
+ _*!covid*_
+_${Cepoetzybot}_akan menampilkan data tentang COVID-19 di Indonesia
 
-🐰*PRIMBON*🐰
+🤖*PRIMBON*🤖
 
-🐼 _*!nama <namakamu>*_
-_${BotName}_🍄akan mencarikan apa arti dari nama kamu
+👾 _*!nama <namakamu>*_
+_${Cepoetzybot}_👾akan mencarikan apa arti dari nama kamu
 
 Contoh : !nama Stalin
 
-🐼 _*!pasangan <namamu & pasangan>*_
-_${BotName}_🍄akan mencarikan kecocokan antara kamu dan pasanganmu
+👾 _*!pasangan <namamu & pasangan>*_
+_${Cepoetzybot}_👾akan mencarikan kecocokan antara kamu dan pasanganmu
 
 Contoh : !pasangan covad & covid
 
 
 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
 FOLLOW INSTAGRAM SAYA
-${instagram}
+${janganlupa}
 
-• TELEGRAM : ${telegram}
-• Youtube : R E U Z
-• WA : 081271699856
-• Pasang Iklan? Chat Whatsapp.
+• Instagram : ${Cepoetznieeh}
+• Nama: Divaldywijaya 
+• WA : +62 822-4612-3503
+• kenalan? chat aja respon kalo ga sibuk hehe.
 
-📺 *IKLAN* : *YOUTUBE REUZ*
-🏚SELALU GUNAKAN MASKER SAAT KELUAR!🏚
-🏚SEMOGA SEHAT SELALU!🏚
+📺 *IKLAN* : *Jangan kebanyakan coli ya🙏🤣*
+😅SELALU SABAR DALAM SEMUA COBAAN YA BUKAN COBAIN!🥴
+🙏SEMOGA SEHAT SELALU!🙏
 `
 }
