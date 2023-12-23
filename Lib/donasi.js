c50d8bf19eb4d0b863f10234b6b9d01855294379
difaldywijaya <153864391+difaldywijaya@users.noreply.github.com>
2023-12-23 21:33:08 +0700

    Update donasi.js

--- Lib/donasi.js
+++ Lib/donasi.js
@@ -1,32 +1,27 @@
 exports.donasi = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🤜🤜{ *MENU DONASI ${BotName}* }🤛🤛
+	return `🤖🤖{ *MENU DONASI ${cepoetzybot}* }🤖🤖
   
   
 TERIMA KASIH
 *${id.split("@s.whatsapp.net")[0]}*
-TELAH MEMBUKA MENU DONASI
-🐼🐼
+TELAH MEMBUKA MENU DONASI🤖
 
 
-🗓*${tampilTanggal}* 🐼
-⏰*${tampilWaktu}* 🐼
+🗓*${tampilTanggal}* 🤖
+⏰*${tampilWaktu}* 🤖
 (Waktu Server)
 
 KALIAN BISA DONASI MENGGUNAKAN
    
-🛡 *PULSA*: 081271699856
-🛡 *OVO*: 081271699856
-🛡 *DANA* : 081271699866
-🛡 *GOPAY* : 081271699856
+🛡 *PULSA*: +62 822-4612-3503
 
-📺*IKLAN* *YOUTUBE REUZ*
+📺*IKLAN* *MakasihAtasDonasinya*
 
 
 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
 FOLLOW INSTAGRAM SAYA
-${instagram}
+${cepoetznieeh}
 
-👾TELEGRAM : ${telegram}
 
 `
 }
